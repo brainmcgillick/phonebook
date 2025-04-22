8dfe732aa19e86470d369508c8989656c776c61e
brainmcgillick <brianmcgillick@gmail.com>
2025-04-22 20:24:49 +0100

    uibuild

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://phonebook-gwq4.onrender.com",
         changeOrigin: true
       }
     }
